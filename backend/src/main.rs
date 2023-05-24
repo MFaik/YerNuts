6435148be46645d344ccc413d83c5985d21e94e5
Tunahan Karatay <gifttak10@gmail.com>
2023-05-25 00:30:51 +0300

    add axum http server with static files and tracing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::SocketAddr, path::PathBuf};
+
+use axum::{response::IntoResponse, routing::get, Router};
+use tower_http::{
+    services::ServeDir,
+    trace::{DefaultMakeSpan, TraceLayer},
+};
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter};
+
+#[tokio::main]
+async fn main() {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new("backend".into(), std::io::stdout);
+
+    let subscriber = tracing_subscriber::registry()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber.");
+
+    let assets_dir = PathBuf::from("./static");
+
+    let app = Router::new()
+        .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
+        .route("/ws", get(ws_handler))
+        // logging so we can see whats going on
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
+
+    tracing::debug!("listening on 127.0.0.1:3000");
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 3000)))
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn ws_handler() -> impl IntoResponse {
+    tracing::info!("Request done on endpoint /ws");
 }
